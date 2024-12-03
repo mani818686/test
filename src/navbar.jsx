fc054bfe7dd4e3e6e897d13d510515e58a3850c2
Manideep <manideep@Mac.lan>
2024-12-02 19:53:41 -0600

    Updated ci.yaml

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
     return (
         <nav className="navbar  border-bottom">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Careers Portal</a>
+                <a className="navbar-brand" href="/">Career Portal</a>
                 <div className="d-flex">
                 <a className="navbar-brand" href="/login">Login</a>
                 <a className="navbar-brand" href="/register">Register</a>
